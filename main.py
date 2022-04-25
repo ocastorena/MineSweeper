667b97eaad11862db0916731db208806033a865f
ocastorena <omarcastorena11@gmail.com>
2022-04-24 20:12:53 -0700

    Create utils.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from tkinter import *
+import settings
+import utils
 
 # override the window settings
 root = Tk()
-root.geometry('1080x720')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("MineSweeper")
 root.resizable(False, False)
 root.configure(bg="black")
@@ -10,19 +12,30 @@ root.configure(bg="black")
 # frame
 top_frame = Frame(
     root,
-    bg="red",
-    width=1080,
-    height=180
+    bg="black",
+    width=settings.WIDTH,
+    height=utils.height_percent(25)
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg="blue",
-    width=216,
-    height=720
+    bg="black",
+    width=utils.width_percent(20),
+    height=settings.HEIGHT
 )
-left_frame.place(x=0, y=180)
+left_frame.place(x=0, y=utils.height_percent(25))
+
+center_frame = Frame(
+    root,
+    bg="black",
+    width=utils.width_percent(80),
+    height=utils.height_percent(75)
+)
+center_frame.place(
+    x=utils.width_percent(20),
+    y=utils.height_percent(25)
+)
 
 # run the window
 root.mainloop()
