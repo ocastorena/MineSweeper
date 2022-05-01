fa71f4b00ed4cc9ff7536eb9d6a014b88068da38
ocastorena <omarcastorena11@gmail.com>
2022-05-01 11:28:06 -0700

    Add game logic

---
+++ cell.py
@@ -0,0 +1,95 @@
+from tkinter import Button, Label
+import random
+
+import settings
+
+
+class Cell:
+    all = []
+    cell_count_label_object = None
+
+    def __init__(self, x, y, is_mine=False):
+        self.is_mine = is_mine
+        self.cell_btn_object = None
+        self.x = x
+        self.y = y
+        Cell.all.append(self)
+
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            width=12,
+            height=4
+        )
+        btn.bind('<Button-1>', self.left_click_actions)
+        btn.bind('<Button-2>', self.right_click_actions)
+        self.cell_btn_object = btn
+
+    @staticmethod
+    def create_cell_count_label(self, location):
+        lbl = Label(
+            location,
+            text=f"Cells left:{settings.CELL_COUNT}"
+        )
+        Cell.cell_count_label_object = lbl
+
+    def left_click_actions(self, event):
+        if self.is_mine:
+            self.show_mine()
+        else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
+            self.show_cell()
+
+    def show_mine(self):
+        self.cell_btn_object.configure(bg="red")
+        print("I am a mine")
+
+    def show_cell(self):
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+
+        return counter
+
+    def get_cell_by_axis(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    def right_click_actions(self):
+        print(self)
+        print("I am right clicked")
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(
+            Cell.all, settings.MINES_COUNT
+        )
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    def __repr__(self):
+        return f"Cell({self.x}, {self.y})"
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
 
@@ -36,6 +37,19 @@ center_frame.place(
     x=utils.width_percent(20),
     y=utils.height_percent(25)
 )
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
+
+# call the label for the last cell class
+Cell.create_cell_count_label(left_frame)
+
+Cell.randomize_mines()
 
 # run the window
 root.mainloop()
